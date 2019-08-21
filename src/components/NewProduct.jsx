2ceaac9ed28d907ff1dd548dfe85b54455813fe6
fr-me-jim <fr.me.jim@gmail.com>
2019-08-21 18:31:36 +0200

    css fixes

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewProduct = () => {
+
+    //state
+    const [ name, setName ] = useState('');
+    const [ price, setPrice ] = useState('');
+
+
+    //Add new product
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validate form
+        if ( name.trim() === '' || price.trim() === '' ) {
+            console.log('error in validation');
+            return;
+        }
+
+        //if all good : 
+
+        //create new product
+
+        //redirect
+
+    }
+    
     return (  
         <div className="row justify-content-center mt-5">
             <div className="col-md-8">
                 <div className="card">
                     <div className="card-body">
-                        <h2 className="text-center mb-4 font-weight-bold ">Add New Book</h2>
-                        <form>
+                        <h2 className="text-center mb-4 font-weight-bold text-dark ">Add New Book</h2>
+                        <form
+                            onSubmit={handleSubmit}
+                        >
                             <div className="form-group">
                                 <label>Book's Name</label>
                                 <input 
                                     type="text" 
                                     className="form-control text-white" 
                                     placeholder="Book's Name" 
+                                    value={name}
+                                    onChange={ e => setName( e.target.value ) }
                                 />
                             </div>
 
@@ -22,7 +50,9 @@ const NewProduct = () => {
                                 <input 
                                     type="text" 
                                     className="form-control text-white" 
-                                    placeholder="Book's price" 
+                                    placeholder="Book's price"
+                                    value={price} 
+                                    onChange={ e => setPrice( e.target.value ) }
                                 />
                             </div>
 
